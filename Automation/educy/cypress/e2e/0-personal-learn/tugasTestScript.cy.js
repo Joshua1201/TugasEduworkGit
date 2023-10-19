2c6e680ec0a7f349abf21b4dae1050132805890f
Joshua1201 <oetomo28@gmail.com>
2023-10-19 22:38:08 +0700

    Tugas Test Script berhasil ditambahkan

---
+++ Automation/educy/cypress/e2e/0-personal-learn/tugasTestScript.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('My First Test', () => {
+    it('clicking "type" shows the right heading', () => {
+        cy.visit('https://example.cypress.io')
+
+        cy.pause()
+
+        cy.contains('type').click()
+
+        cy.url().should('include', 'https://example.cypress.io/commands/actions')
+
+        cy.get('.action-email').type('joshua@gmail.com').should('have.value', 'joshua@gmail.com')
+    });
+});
