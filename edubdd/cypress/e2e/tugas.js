be27d77bdcc4f04bd0392556bfc5b37527419c31
Joshua1201 <oetomo28@gmail.com>
2023-10-27 20:46:42 +0700

    Tugas Run Test berhasil ditambahkan

--- edubdd/cypress/e2e/tugas.js
+++ edubdd/cypress/e2e/tugas.js
@@ -8,26 +8,22 @@ Given('I am on the homepage', () => {
     cy.get('strong').should('contain.text', 'Home')
 })
 
-//Search for available queries
-When('I type any available queries on the Search bar', () => {
-    SearchPage.fillSearchBar('bank')
-})
+When('I type {string} on the Search bar', (query) => {
+    SearchPage.fillSearchBar(query)
+});
 
 When('I click Enter on my keyboard', () => {
     SearchPage.fillSearchBar('{enter}')
     cy.url().should('include', 'search.html')
 })
 
+//Search for available queries
 Then('I should see some available pages on the Search Results', () => {
     cy.get('h2').should('contain.text', 'Search Results')
     cy.get('a').should('contain.text', 'Zero - Free Access to Online Banking')
 })
 
 //Search for unavailable queries
-When('I type any unavailable queries on the Search bar', () => {
-    SearchPage.fillSearchBar('banks')
-})
-
 Then('I should not see any available page on the Search Results', () => {
     cy.get('h2').should('contain.text', 'Search Results')
     cy.get('div').should('contain.text', 'No results were found for the query: banks')
